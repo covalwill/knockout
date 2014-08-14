4e1288aecba188a516879babc25ee72412d3b9f3
covalwill <klimtgit@gmail.com>
2014-08-14 12:36:10 -0700

    Added validations for description

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -72,4 +72,21 @@ it "displays an error when todo list has no description" do
 		expect(page).to_not have_content("Grocery list")
 	end
 
+it "displays an error when todo list has no description" do
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+
+		fill_in "Title", with: "Grocery list"
+		fill_in "Description", with: "Food"
+		click_button "Create Todo list"
+
+		expect(page).to have_content("error")
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		expect(page).to_not have_content("Grocery list")
+	end
 end
